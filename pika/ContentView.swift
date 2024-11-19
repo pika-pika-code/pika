617dbb6277fde04c3fdddc0a8cdd7a005a3a70b4
pika-pika-code <a24.adeg@g.chuo-u.ac.jp>
2024-11-19 17:29:07 +0900

    Initial Commit

---
+++ pika/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pika
+//
+//  Created by 清水　陽歌里  on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pika/pikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pikaApp.swift
+//  pika
+//
+//  Created by 清水　陽歌里  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct pikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
